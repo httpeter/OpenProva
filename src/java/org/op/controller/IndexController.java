4377dcb8d38ba68adc1e5faf7115e814eeebb7a3
PeterH <PeterH@Porter.furore.com>
2015-12-21 17:40:14 +0100

    removed logger

--- src/java/org/op/controller/IndexController.java
+++ src/java/org/op/controller/IndexController.java
@@ -5,7 +5,6 @@ import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
 import javax.faces.context.ExternalContext;
 import javax.faces.context.FacesContext;
-import javax.servlet.http.HttpSession;
 import org.op.util.FMessage;
 
 @ManagedBean
